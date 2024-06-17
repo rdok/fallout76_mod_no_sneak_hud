909adcc2ce276651d8e30f05a3d2181e85fd9385
Rizart Dokollari <r.dokollari@gmail.com>
2024-06-18 00:16:08 +0100

    fix: Use correct path for mod.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,5 +1,5 @@
 $archive2Path = Join-Path $PSScriptRoot "../libraries/Quick Configuration v1.12.7/Archive2/Archive2.exe"
-$srcPath = Join-Path $PSScriptRoot "../src/interface/"
+$srcPath = Join-Path $PSScriptRoot "../src"
 
 $packageJsonPath = Join-Path -Path $PSScriptRoot -ChildPath "..\package.json"
 $jsonContent = Get-Content -Path $packageJsonPath -Raw
@@ -12,7 +12,7 @@ if (-Not (Test-Path -Path $archive2Path)) {
 
 & $archive2Path $srcPath -create="dist/NoStealthMeter.ba2"
 
-$buildName="no_stealth_meter_${version}.7z"
+$buildName="No Stealth Meter ${version}.7z"
 $ba2Name="NoStealthMeter.ba2"
 Remove-Item -LiteralPath $buildName -Force -Recurse
 .\node_modules\7z-bin\win32\7z.exe a $buildName ".\dist\${ba2Name}"
--- scripts/build_dev_deploy.ps1
+++ scripts/build_dev_deploy.ps1
@@ -1,20 +1,24 @@
+$buildScriptPath =  Join-Path -Path $PSScriptRoot -ChildPath ".\build.ps1"
+& $buildScriptPath
+
 $packageJsonPath = Join-Path -Path $PSScriptRoot -ChildPath "..\package.json"
 $jsonContent = Get-Content -Path $packageJsonPath -Raw
 $jsonObject = $jsonContent | ConvertFrom-Json
 $version = $jsonObject.version
 $buildName="no_stealth_meter_${version}.7z"
 $ba2Name="NoStealthMeter.ba2"
-$distPath = Join-Path $PSScriptRoot "../dist/$ba2Name"
+$ba2SourceFilename = Join-Path $PSScriptRoot "../dist/$ba2Name"
 
 $gameFilename="D:\SteamLibrary\steamapps\common\Fallout76"
 if (-Not (Test-Path -Path $gameFilename)) {
     throw "The game installation path '$gameFilename' does not exist. Please update the game installation path accordingly."
 }
 
-$modFilename="$gameFilename\Data\$ba2Name"
+$ba2DestinationFilename="$gameFilename\Data\$ba2Name"
 
-if (-Not (Test-Path -Path $modFilename)) {
-    throw "The mod file '$modFilename' does not exist. Please build the mod through the build.ps1 script, deploy mod through Vortex; and then try again."
+if (-Not (Test-Path -Path $ba2DestinationFilename)) {
+    throw "The mod file '$ba2DestinationFilename' does not exist. Please build the mod through the build.ps1 script, deploy mod through Vortex; and then try again."
 }
 
-Copy-Item -Path $distPath -Destination $modFilename
+Remove-Item -LiteralPath $ba2DestinationFilename -Force -Recurse
+Copy-Item -Force -Path $ba2SourceFilename -Destination $ba2DestinationFilename
